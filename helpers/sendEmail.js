9a9c5b6d46eaa751d8fe96bb49d6e391483428ea
timatas <139263028+timatas@users.noreply.github.com>
2024-05-14 14:55:04 +0300

    add create update delete Waters

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -8,15 +8,19 @@ const { MAILTRAP_PASSWORD } = process.env;
 const email = {
   host: "sandbox.smtp.mailtrap.io",
   port: 2525,
+  // auth: {
+  //   user: "431b14cc250a3e",
+  //   pass: MAILTRAP_PASSWORD,
+  // },
   auth: {
-    user: "431b14cc250a3e",
-    pass: MAILTRAP_PASSWORD,
+    user: "330d7d4216cee7",
+    pass: "0eef049e235ae0",
   },
 };
 const transport = nodemailer.createTransport(email);
 
 export const sendEmail = async (data) => {
-  const email = { ...data, from: "431b14cc250a3e" };
+  const email = { ...data, from: "ekonomistleshoz@gmail.com" };
   try {
     await transport.sendMail(email);
     return true;
