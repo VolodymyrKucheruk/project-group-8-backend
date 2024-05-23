1ba1bdf20e378a19fbe5dc89d9909fe2fb565758
timatas <139263028+timatas@users.noreply.github.com>
2024-05-24 00:16:10 +0300

    get monthly

--- services/waterServices.js
+++ services/waterServices.js
@@ -21,87 +21,25 @@ export async function updatesWater(req, id, body) {
 
 export async function removeWater(req, id) {
   const { _id: owner } = req.user;
+
   const removedWater = await Water.findOneAndDelete({
     _id: id,
     owner,
   });
+
   if (!removedWater) {
     return null;
   }
-  return removedWater;
+
+  const updatedWaterArray = await Water.find({ owner });
+
+  return updatedWaterArray;
 }
-//=======
-//стара відповідь за місяць
-//{ "2024-05-21": { "totalAmount": 14352, "percentage": 957 }, "2024-05-20": { "totalAmount": 900, "percentage": 60 }, "2024-05-22": { "totalAmount": 2050, "percentage": 137 } }
-// export async function listMonth(req) {
-//   const { _id: owner, dailyWaterNorma } = req.user;
 
-//   const { dateDose } = req.body;
-
-//   const year = dateDose.substring(0, 4);
-//   const month = dateDose.substring(5, 7);
-
-//   const list = await Water.aggregate([
-//     { $match: { owner, dateDose: { $regex: `^${year}-${month}` } } },
-//     { $unset: ["createdAt", "updatedAt"] },
-//     { $sort: { timeDose: 1 } },
-//     {
-//       $group: {
-//         _id: "$dateDose",
-//         totalAmount: { $sum: "$amountDose" },
-//       },
-//     },
-//   ]);
-
-//   const dailyList = {};
-//   list.forEach((item) => {
-//     const date = item._id;
-//     const totalAmount = item.totalAmount;
-
-//     const percentage = Math.round((totalAmount / dailyWaterNorma) * 100);
-
-//     dailyList[date] = { totalAmount, percentage };
-//   });
-
-//   return dailyList;
-// }
-
-//=======
-//масив даних за місяць:[{"dateDose":"2024-05-21","totalAmount":14352,"percentage":957},{"dateDose":"2024-05-20","totalAmount":900,"percentage":60},{"dateDose":"2024-05-22","totalAmount":2050,"percentage":137}]
-export async function listMonth(req) {
-  const { _id: owner, dailyWaterNorma } = req.user;
-  const { dateDose } = req.body;
-
-  const year = dateDose.substring(0, 4);
-  const month = dateDose.substring(5, 7);
-
-  const list = await Water.aggregate([
-    { $match: { owner, dateDose: { $regex: `^${year}-${month}` } } },
-    { $unset: ["createdAt", "updatedAt"] },
-    { $sort: { timeDose: 1 } },
-    {
-      $group: {
-        _id: "$dateDose",
-        totalAmount: { $sum: "$amountDose" },
-      },
-    },
-  ]);
-
-  const dailyList = list.map((item) => {
-    const dateDose = item._id;
-    const totalAmount = item.totalAmount;
-    const percentage = Math.round((totalAmount / dailyWaterNorma) * 100);
-
-    return { dateDose, totalAmount, percentage };
-  });
-
-  return dailyList;
-}
-//==========
 export async function listMonthByDay(req) {
   const { _id: owner, dailyWaterNorma } = req.user;
-  const { date } = req.params; // Отримуємо дату з параметрів маршруту
-
+  const { date } = req.params;
+  console.log(req.params);
   const year = date.substring(0, 4);
   const month = date.substring(5, 7);
 
@@ -122,13 +60,12 @@ export async function listMonthByDay(req) {
     const totalAmount = item.totalAmount;
     const percentage = Math.round((totalAmount / dailyWaterNorma) * 100);
 
-    return { dateDose, totalAmount, percentage };
+    return { dateDose, percentage };
   });
 
   return monthlyList;
 }
 
-//==========
 //відповідь масив за день [{"timeDose":"23:01","amountDose":500,"percentage":33},{"timeDose":"23:55","amountDose":400,"percentage":27}]
 export async function listDay(req) {
   const { _id: owner, dailyWaterNorma } = req.user;
@@ -145,43 +82,13 @@ export async function listDay(req) {
 
   return dailyList;
 }
-//==========
-//минула відповідь за день {"list":[{"amount":500,"time":"23:01"},{"amount":400,"time":"23:55"}],"totalWater":900,"percent":60}
-// export async function listDay(req) {
-//   const { _id: owner, dailyWaterNorma } = req.user;
 
-//   const { dateDose } = req.body;
-
-//   const list = await Water.aggregate([
-//     {
-//       $match: { owner, dateDose },
-//     },
-//     {
-//       $group: {
-//         _id: null,
-//         totalWater: { $sum: "$amountDose" },
-//         doses: { $push: { amount: "$amountDose", time: "$timeDose" } },
-//       },
-//     },
-//   ]);
-
-//   const { totalWater } = list.length > 0 ? list[0] : { totalWater: 0 };
-
-//   const percent = (totalWater / dailyWaterNorma) * 100;
-
-//   return {
-//     list: list[0]?.doses || [],
-//     totalWater,
-//     percent,
-//   };
-// }
-//===========
 //відповідь за датою з фронтенду [{"timeDose":"23:01","amountDose":500,"percentage":33},{"timeDose":"23:55","amountDose":400,"percentage":27}]
 export async function listDate(req) {
   const { _id: owner, dailyWaterNorma } = req.user;
-  const date = req.params.date; // Отримання значення дати з параметрів шляху
+  const date = req.params.date;
 
-  const list = await Water.find({ owner, dateDose: date }); // Фільтрація за датою date
+  const list = await Water.find({ owner, dateDose: date });
 
   const dailyList = list.map((item) => {
     const { timeDose, amountDose } = item;
